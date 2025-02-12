71e944f745254c40e20abc7211264e3f4c1acc4f
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2025-02-12 09:01:00 -0300

    Add files via upload

--- contratos/contratos.js
+++ contratos/contratos.js
@@ -135,10 +135,10 @@ function criarplanilha() {
             <td data-label="Objeto">${objeto}</td>
             <td data-label="Empresa">${empresa}</td>
             <td data-label="Menor Prazo">${prazo}</td>
-            <td data-label="Prazo de Execução">${execucaoFormatada} dias</td>
-            <td data-label="Dias Restantes para Execução" class="numero">${diasRestantesExecucao !== null ? diasRestantesExecucao : "Não definido"}</td>
-            <td data-label="Prazo de Vigência">${vigenciaFormatada} dias</td>
-            <td data-label="Dias Restantes para Vigência" class="numero">${diasRestantesVigencia !== null ? diasRestantesVigencia : "Não definido"}</td>
+            <td data-label="Prazo de Execução">${execucaoFormatada}</td>
+            <td data-label="Dias Restantes para Execução" class="numero">${diasRestantesExecucao !== null ? diasRestantesExecucao : "Não definido"} dias</td>
+            <td data-label="Prazo de Vigência">${vigenciaFormatada}</td>
+            <td data-label="Dias Restantes para Vigência" class="numero">${diasRestantesVigencia !== null ? diasRestantesVigencia : "Não definido"} dias</td>
         `;
 
         // Adicionar linha à tabela
